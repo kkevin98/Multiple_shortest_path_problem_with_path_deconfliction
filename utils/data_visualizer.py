e7096a8ac322d2056fdd686891b100cce4f8d4fe
Kevin Marzio <marzio92@gmail.com>
2023-03-31 10:04:42 +0200

    Update data_visualazer module
    Now to plot we use a np.ndarray instead of a gb.MVar such that we can
    also plot results stored in a np.ndarray (as in MSPP-PD(NQP) notebook).
    
    Removed the annotations in the plot for the agents' source and terminus
    for clearer plots.
    
    Add module description for clarity

--- utils/data_visualizer.py
+++ utils/data_visualizer.py
@@ -1,4 +1,4 @@
-"""Module description"""
+"""Module that contains functions to plot the solution of a MSPP or a MSPP-PD"""
 from . import np, math, plt, cm, ConnectionPatch
 
 
@@ -48,18 +48,16 @@ def _plot_agent_s_t(ax, agent, node_to_location, param_dict):
 
     ax.plot(*node_to_location[agent.source], **
             param_dict, label=f"Agent{agent.idx}")
-    ax.annotate(
-        f"s{agent.idx}", node_to_location[agent.source], color=param_dict.get("color"))
     ax.plot(*node_to_location[agent.terminus], **param_dict)
-    ax.annotate(
-        f"t{agent.idx}", node_to_location[agent.terminus], color=param_dict.get("color"))
 
 
-def _plot_agent_path(ax, X, w_arcs, agent, node_to_location, param_dict):
-    """Function to plot the path of an agent
+def _plot_agent_path(ax, x, w_arcs, agent, node_to_location, param_dict):
+    """Function to plot the optimal path of an agent
 
     Args:
         ax (matplotlib.axes.Axes): Axes object where to plot the 2 nodes
+        x (np.ndarray): a (2,2) matrix containing the solution of a MSPP or a MSPP-PD
+        w_arcs (list): list of weighted arcs (WArc) in the network
         agent (Agent): agent whose path we want to print
         node_to_location (dict): dictionary that tells on which (x,y) location
           in the axes' plane a node has to be placed 
@@ -67,21 +65,24 @@ def _plot_agent_path(ax, X, w_arcs, agent, node_to_location, param_dict):
     """
 
     for arc in w_arcs:
-        if math.isclose(X.x[arc.idx, agent.idx], 1):
+        if math.isclose(x[arc.idx, agent.idx], 1):
             arrow = ConnectionPatch(node_to_location[arc.i],
                                     node_to_location[arc.j],
                                     **param_dict)
             ax.add_artist(arrow)
 
 
-def plot_solution(X, nodes_grid, w_arcs, agents):
-    """Function to plot the solution of a MSPP or MSPP-PD
+def plot_solution(x, nodes_grid, w_arcs, agents, title=None):
+    """Function to plot the optimal solution of a MSPP or MSPP-PD
 
     Args:
-        X (gb.MVar): x variables resulting from the solution of a MSPP or a MSPP-PD
-        nodes (list): list of the nodes in the network
+        x (np.ndarray): a (2,2) matrix containing the solution of a MSPP or a MSPP-PD
+        nodes_grid (np.ndarray): a (2,2) matrix containing the nodes of the grid-like
+          network
         w_arcs (list): list of weighted arcs (WArc) in the network
         agents (list): list of routed agents (Agent)
+        title (str): string representing the title of the plot. By default the plot
+          does not have a title
     """
 
     # create the figure and an iterator over different colors. Each agent will have its own color
@@ -106,7 +107,7 @@ def plot_solution(X, nodes_grid, w_arcs, agents):
         agent_color = next(color)
         _plot_agent_s_t(ax, agent, node_to_location,
                         {"marker": "o", "markersize": 7.5, "color": agent_color, "alpha": 0.8})
-        _plot_agent_path(ax, X, w_arcs, agent, node_to_location,
+        _plot_agent_path(ax, x, w_arcs, agent, node_to_location,
                          {"coordsA": "data", "coordsB": "data", "shrinkA": 5, "shrinkB": 5,
                           "arrowstyle": "-|>", "color": agent_color, "linewidth": 2, "alpha": 0.8})
 
@@ -114,5 +115,6 @@ def plot_solution(X, nodes_grid, w_arcs, agents):
     ax.legend(bbox_to_anchor=(1, 1))
     ax.xaxis.set_ticks(range(num_cols))
     ax.yaxis.set_ticks(range(num_rows))
+    ax.set_title(title)
 
     plt.show()
